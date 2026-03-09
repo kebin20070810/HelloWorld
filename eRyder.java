53e029eec3b4d7ab0c3513c15141eb12f0fd7bee
kebin20070810 <3530879551@qq.com>
2026-03-09 12:34:50 +0800

    Remove unnecessary Main class from eRyder

--- eRyder.java
+++ eRyder.java
@@ -68,15 +68,7 @@ public class eRyder {
     public void setKmDriven(double kmDriven) {
         this.kmDriven = kmDriven;
     }
-    public class Main {
-    public static void main(String[] args) {
-        eRyder bike1 = new eRyder();
-        bike1.printBikeDetails();
+    
+}
 
-        eRyder bike2 = new eRyder("bike1", 85, true, 125.5);
-        bike2.ride();
-        bike2.printBikeDetails();
-    }
-}
-}
 
